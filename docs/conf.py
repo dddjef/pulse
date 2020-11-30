8cdbf01efc869d65e6afb09fc056a954840ae6d4
dddjef <dddjef@hotmail.com>
2020-11-30 20:22:04 +0100

    change the module path in doc config to add python folder

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@
 # sys.path.insert(0, os.path.abspath('.'))
 import sys
 import os
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../python'))
 
 # -- Project information -----------------------------------------------------
 
